c831b2c25fc75ce7a93a38281c24f76c4f48e33e
aki <igorakimy@gmail.com>
2024-01-27 04:11:11 +0300

    reflection slices

--- reflection/walk.go
+++ reflection/walk.go
@@ -17,6 +17,13 @@ type Profile struct {
 func Walk(x any, fn func(string)) {
 	val := getValue(x)
 
+	if val.Kind() == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
+			Walk(val.Index(i).Interface(), fn)
+		}
+		return
+	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
