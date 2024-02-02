6b7f9e0ea191dd29de5fb0e9ded2906d3b721d38
aki <igorakimy@gmail.com>
2024-02-02 16:29:13 +0300

    refactoring reading files

--- reading_files/blogposts.go
+++ reading_files/blogposts.go
@@ -1,6 +1,7 @@
 package reading_files
 
 import (
+	"io"
 	"io/fs"
 )
 
@@ -15,8 +16,29 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			//todo: needs clarification, should we totally fail if one file fails? or just ignore?
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer func(postFile fs.File) { _ = postFile.Close() }(postFile)
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
--- reading_files/blogposts_test.go
+++ reading_files/blogposts_test.go
@@ -2,14 +2,15 @@ package reading_files_test
 
 import (
 	"github.come/igorakimy/go_with_tests/reading_files"
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
 
 func TestBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello_world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello_world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
 	posts, err := reading_files.NewPostsFromFS(fs)
@@ -20,5 +21,24 @@ func TestBlogPosts(t *testing.T) {
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello_world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
+	}
+
+	posts, err := reading_files.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := posts[0]
+	want := reading_files.Post{Title: "Post 2"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
