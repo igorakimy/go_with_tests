154170a150f8d406a7dedc6cd83196069820f8f1
aki <igorakimy@gmail.com>
2024-02-05 13:35:23 +0300

    application server another test

--- application/server.go
+++ application/server.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprint(w, "20")
+
+	player := strings.TrimPrefix(r.URL.String(), "/players/")
+
+	if player == "Pepper" {
+		_, _ = fmt.Fprint(w, "20")
+		return
+	} else {
+		_, _ = fmt.Fprint(w, "10")
+		return
+	}
 }
