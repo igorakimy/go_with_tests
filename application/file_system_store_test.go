4f22cdb3c550b4ccbc5c10a7e65ebc4899e00a97
aki <igorakimy@gmail.com>
2024-02-11 14:49:20 +0300

    refactoring application io file system store

--- application/file_system_store_test.go
+++ application/file_system_store_test.go
@@ -8,11 +8,7 @@ import (
 func TestFileSystemStore(t *testing.T) {
 
 	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
-
-		store := FileSystemPlayerStore{database}
+		store := getStore()
 
 		got := store.GetLeague()
 
@@ -28,18 +24,27 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
+		store := getStore()
+
+		got := store.GetPlayerScore("Chris")
+		want := 33
+		assertScoreEquals(t, got, want)
+	})
+}
+
+func getStore() FileSystemPlayerStore {
+	database := strings.NewReader(`[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
 
-		store := FileSystemPlayerStore{database}
+	store := FileSystemPlayerStore{database}
 
-		got := store.GetPlayerScore("Chris")
+	return store
+}
 
-		want := 33
-
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
-	})
+func assertScoreEquals(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
 }
