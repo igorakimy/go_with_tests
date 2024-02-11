a6ee40462ecff226eafd35238896e6031efc4be8
aki <igorakimy@gmail.com>
2024-02-11 15:48:54 +0300

    application io file system store create temp file

--- application/file_system_store_test.go
+++ application/file_system_store_test.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 
 	t.Run("league from a reader", func(t *testing.T) {
-		store := getStore()
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
 
 		got := store.GetLeague()
 
@@ -19,12 +25,18 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertLeague(t, got, want)
 
+		// gain again
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		store := getStore()
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Chris")
 		want := 33
@@ -32,14 +44,27 @@ func TestFileSystemStore(t *testing.T) {
 	})
 }
 
-func getStore() FileSystemPlayerStore {
-	database := strings.NewReader(`[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
 
-	store := FileSystemPlayerStore{database}
+	tmpFile, err := os.CreateTemp("", "db")
 
-	return store
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	_, err = tmpFile.Write([]byte(initialData))
+
+	if err != nil {
+		t.Fatalf("could not write the file %v", err)
+	}
+
+	removeFileFunc := func() {
+		_ = tmpFile.Close()
+		_ = os.Remove(tmpFile.Name())
+	}
+
+	return tmpFile, removeFileFunc
 }
 
 func assertScoreEquals(t testing.TB, got, want int) {
