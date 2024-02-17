a22b21272ce7fe8a748b01846087cf88b94ad6f5
aki <igorakimy@gmail.com>
2024-02-17 15:50:54 +0300

    application io file system tape test

--- application/tape.go
+++ application/tape.go
@@ -1,12 +1,15 @@
 package main
 
-import "io"
+import (
+	"os"
+)
 
 type tape struct {
-	file io.ReadWriteSeeker
+	file *os.File
 }
 
 func (t *tape) Write(p []byte) (n int, err error) {
+	_ = t.file.Truncate(0)
 	_, _ = t.file.Seek(0, 0)
 	return t.file.Write(p)
 }
